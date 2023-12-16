a68f2b1a37e877ba8f3f9c523bfc43596a5973d6
Arunabha Mukhopadhyay <145916970+Arunabha-Mukhopadhyay@users.noreply.github.com>
2023-12-16 20:19:30 +0530

    Add files via upload

---
+++ demo2.js
@@ -0,0 +1,8 @@
+let num = 50 ;
+let userNum = prompt("enter the no:");
+
+while (userNum != num) {
+  userNum = prompt("not correct. guess again: ");
+}
+
+console.log("congrats you have guessed the correct no:")
